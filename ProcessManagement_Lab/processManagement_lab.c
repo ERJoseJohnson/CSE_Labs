618c05990e581710f37845ae596ca5dd93d0f0cc
ERJoseJohnson <jose.johnson.jose@gmail.com>
2020-02-26 17:46:03 +0800

    feat: TODO#3 done

--- ProcessManagement_Lab/processManagement_lab.c
+++ ProcessManagement_Lab/processManagement_lab.c
@@ -10,6 +10,7 @@ void task(long duration)
     usleep(duration*TIME_MULTIPLIER);
 
     // TODO: protect the access of shared variable below
+    sem_wait(sem_global_data);
     // update global variables to simulate statistics
     ShmPTR_global_data->sum_work += duration;
     ShmPTR_global_data->total_tasks ++;
@@ -22,6 +23,7 @@ void task(long duration)
     if (duration > ShmPTR_global_data->max) {
         ShmPTR_global_data->max = duration;
     }
+    sem_post(sem_global_data);
 }
 
 
@@ -35,10 +37,30 @@ void job_dispatch(int i){
     //          c. If there's new job, execute the job accordingly: either by calling task(), usleep, exit(3) or kill(getpid(), SIGKILL)
     //          d. Loop back to check for new job 
 
+    //printf("Hello from child %d with pid %d and parent id %d\n", i, getpid(), getppid());
+    //exit(0); 
 
-    printf("Hello from child %d with pid %d and parent id %d\n", i, getpid(), getppid());
-    exit(0); 
-
+    while(true){
+        sem_wait(sem_jobs_buffer[i]);
+        if(shmPTR_jobs_buffer[i].task_status != 0){
+            if(shmPTR_jobs_buffer[i].task_type == 't'){
+                task(shmPTR_jobs_buffer[i].task_duration);
+                shmPTR_jobs_buffer[i].task_status = 0;
+                shmPTR_jobs_buffer[i].task_duration = 0;
+            }
+            else if(shmPTR_jobs_buffer[i].task_type == 'w'){
+                usleep(shmPTR_jobs_buffer[i].task_duration * TIME_MULTIPLIER);
+                shmPTR_jobs_buffer[i].task_status = 0;
+                shmPTR_jobs_buffer[i].task_duration = 0;
+            }
+            else if(shmPTR_jobs_buffer[i].task_type == 'z'){
+                exit(3);
+            }
+            else if(shmPTR_jobs_buffer[i].task_type == 'i'){
+                kill(getpid(),SIGKILL);
+            }
+        }
+    }
 }
 
 /** 
@@ -120,8 +142,6 @@ void setup(){
     */
 
     //Semaphore for number_of_processes
-    //char semaphoreName[8] = "semjobsi";
-    //char tempNumString;
     for(int i=0;i<number_of_processes;i++){
         char *sem_name = malloc(sizeof(char)*16);
         sprintf(sem_name, "semjobs%d", i);
@@ -156,7 +176,7 @@ void createchildren(){
     Task A
     */
     for(int i = 0; i<number_of_processes; i++){
-        int temp_pid = fork();
+        int temp_pid = fork();          //Child process id sent to parent, so use in else
         if (temp_pid<0){
             printf("%d child was not created", i);
         }
@@ -220,7 +240,7 @@ void cleanup(){
     // 3. Unlink all semaphores in sem_jobs_buffer
 }
 
-// Test main for TODO#1
+// Test main for TODO#3
 int main(int argc, char* argv[]){
 
     //Check and parse command line options to be in the right format
@@ -242,14 +262,76 @@ int main(int argc, char* argv[]){
     printf("Number of processes: %d\n", number_of_processes);
     printf("Main process pid %d \n", getpid());
 
+    setup();
     createchildren();
-    
+
+    //enter exactly N jobs to the buffer
     for (int i = 0; i<number_of_processes; i++){
-        printf("Child process %d created with pid: %d \n", i, children_processes[i]);
-        wait(NULL);
+        shmPTR_jobs_buffer[i].task_type = 't';
+        shmPTR_jobs_buffer[i].task_duration = 1;
+        shmPTR_jobs_buffer[i].task_status = 1; //new, undone job
+        sem_post(sem_jobs_buffer[i]); // signal the child
+    }
+
+
+    //sleep for 3 seconds, the children processes should all finish by now
+    sleep(3);
+
+    //enter exactly N termination jobs to the buffer
+    for (int i = 0; i<number_of_processes; i++){
+        shmPTR_jobs_buffer[i].task_type = 'z';//termination job
+        shmPTR_jobs_buffer[i].task_duration = 1;
+        shmPTR_jobs_buffer[i].task_status = 1; 
+        sem_post(sem_jobs_buffer[i]); // signal the child
+    }
+
+    //wait for all N children processes
+    int waitpid_result;
+    for (int i = 0; i<number_of_processes; i++){
+        waitpid_result = waitpid(children_processes[i], NULL, 0); // returns when child exits normally
+        if (waitpid_result != -1){
+            printf("Child %d with pid %d has exited successfully\n", i, waitpid_result);
+        }
+    }
+
+        // print final results
+    printf("Final results: sum -- %ld, odd -- %ld, min -- %ld, max -- %ld, total task -- %ld\n", ShmPTR_global_data->sum_work, ShmPTR_global_data->odd, ShmPTR_global_data->min, ShmPTR_global_data->max, ShmPTR_global_data->total_tasks);
+
+    //detach and remove shared memory locations
+    int detach_status = shmdt((void *) ShmPTR_global_data); //detach
+    if (detach_status == -1) printf("Detach shared memory global_data ERROR\n");
+    int remove_status = shmctl(ShmID_global_data, IPC_RMID, NULL); //delete
+    if (remove_status == -1) printf("Remove shared memory global_data ERROR\n");
+    detach_status = shmdt((void *) shmPTR_jobs_buffer); //detach
+    if (detach_status == -1) printf("Detach shared memory jobs ERROR\n");
+    remove_status = shmctl(ShmID_jobs, IPC_RMID, NULL); //delete
+    if (remove_status == -1) printf("Remove shared memory jobs ERROR\n");
+
+
+    //unlink all semaphores before exiting process
+    int sem_close_status = sem_unlink("semglobaldata");
+    if (sem_close_status == 0){
+        printf("Semaphore globaldata closes succesfully.\n");
+    }
+    else{
+        printf("Semaphore globaldata fails to close.\n");
+    }
+
+    for (int i = 0; i<number_of_processes; i++){
+        char *sem_name = malloc(sizeof(char)*16);
+        sprintf(sem_name, "semjobs%d", i);
+        sem_close_status = sem_unlink(sem_name);
+        if (sem_close_status == 0){
+             printf("Semaphore jobs %d closes succesfully.\n", i);
+        }
+        else{
+            printf("Semaphore jobs %d fails to close.\n", i);
+        }
+        free(sem_name);
     }
 
 
     printf("success\n");
+    
     return 0;
 }
